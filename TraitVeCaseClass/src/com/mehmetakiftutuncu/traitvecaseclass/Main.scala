daf685126837e1069c1078e925167bda2ad44552
Mehmet Akif Tütüncü <m.akif.tutuncu@gmail.com>
2016-01-24 11:50:53 +0200

    Trait ve class ile ilgili örnek eklendi.

--- TraitVeCaseClass/src/com/mehmetakiftutuncu/traitvecaseclass/Main.scala
+++ TraitVeCaseClass/src/com/mehmetakiftutuncu/traitvecaseclass/Main.scala
@@ -5,6 +5,14 @@ package com.mehmetakiftutuncu.traitvecaseclass
   */
 object Main {
   def main(args: Array[String]) {
-    
+    val ahmet = new Ahmet
+    val mehmet = new Mehmet
+    val ali = new Ali
+
+    ahmet.cal()
+    mehmet.cal()
+    ali.cal()
+    ali.sorunuCoz("kimya")
+    ali.sorunuCoz("bilgisayar")
   }
 }
